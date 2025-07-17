7ce1fbb25ac09d9144e8349a2de35456b1669898
jaxavenger2007@gmail.com <jaxavenger2007@gmail.com>
2025-07-17 04:39:37 +0000

    Created AccountInformation, TimeTablePage and DashBoardPage; Deleted Form2, Form1 and Form3

--- client_code/AccountInformation/__init__.py
+++ client_code/AccountInformation/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import Form3Template
+from ._anvil_designer import AccountInformationTemplate
 from anvil import *
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class Form3(Form3Template):
+class AccountInformation(AccountInformationTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)

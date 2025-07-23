bee57e9b1fb9bcf8718c2e59bd080293f2efd8b4
jaxavenger2007@gmail.com <jaxavenger2007@gmail.com>
2025-07-23 03:24:18 +0000

    Edited HomePage, settings, AccountInformation and TimeTablePage; Deleted DashBoardPage

--- client_code/HomePage/__init__.py
+++ client_code/HomePage/__init__.py
@@ -11,3 +11,11 @@ class HomePage(HomePageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def time_table_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    open_form('TimeTablePage')
+
+  def primary_color_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('AccountInformation')
--- client_code/TimeTablePage/__init__.py
+++ client_code/TimeTablePage/__init__.py
@@ -11,3 +11,7 @@ class TimeTablePage(TimeTablePageTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def homepage_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('HomePage')

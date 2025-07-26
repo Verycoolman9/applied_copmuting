4ade8095252b28b2fcff8b913d8e6b915dbdc14a
jaxavenger2007@gmail.com <jaxavenger2007@gmail.com>
2025-07-26 01:11:15 +0000

    Created StudentSignup and TeacherSignup; Edited StudentOrTeacher

--- client_code/StudentOrTeacher/__init__.py
+++ client_code/StudentOrTeacher/__init__.py
@@ -12,3 +12,11 @@ class StudentOrTeacher(StudentOrTeacherTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_2_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('TeacherSignup')
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('StudentSignup')

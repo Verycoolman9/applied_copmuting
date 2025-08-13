553054da35c632fee2e512d1d07e08c40664e337
jaxavenger2007@gmail.com <jaxavenger2007@gmail.com>
2025-08-13 02:44:04 +0000

    Edited StudentOrTeacher and StudentSignup

--- client_code/StudentOrTeacher/__init__.py
+++ client_code/StudentOrTeacher/__init__.py
@@ -5,6 +5,8 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
+ALLOWED_STUDENT_EMAIL = "@schools.vic.edu.au"
+ALLOWED_TEACHER_EMAIL = "@education.vic.gov.au"
 
 class StudentOrTeacher(StudentOrTeacherTemplate):
   def __init__(self, **properties):
@@ -13,10 +15,14 @@ class StudentOrTeacher(StudentOrTeacherTemplate):
 
     # Any code you write here will run before the form opens.
 
-  def button_2_click(self, **event_args):
+  def student_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('TeacherSignup')
+    anvil.users.login_with_form()
+    if ALLOWED_STUDENT_EMAIL is not ALLOWED_STUDENT_EMAIL.endswith("@schools.vic.edu.au"):
+      alert("Please use student Email when signing up or logging in :)")
 
-  def button_1_click(self, **event_args):
+  def teacher_button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    open_form('StudentSignup')
+    anvil.users.login_with_form()
+    if ALLOWED_TEACHER_EMAIL is not ALLOWED_TEACHER_EMAIL.endswith("@education.vic.edu.au"):
+      alert("Please use teacher email when signing up or logging in")
--- client_code/StudentSignup/__init__.py
+++ client_code/StudentSignup/__init__.py
@@ -4,6 +4,10 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from anvil.users import login_with_email, signup_with_email
+
+
+ALLOWED_DOMAIN = "@schools.vic.edu.au"
 
 
 class StudentSignup(StudentSignupTemplate):
@@ -12,3 +16,15 @@ class StudentSignup(StudentSignupTemplate):
     anvil.users.login_with_form()
     print(f"This user has logged in: {anvil.users.get_user()['email']}")
     open_form('StudentHomePage')
+
+@anvil.server.callable
+def restricted_signup(email, password):
+  if not email.lower().endswith(ALLOWED_DOMAIN):
+    raise Exception(f"Only emails ending in {ALLOWED_DOMAIN} are allowed.")
+  return signup_with_email(email, password)
+
+@anvil.server.callable
+def restricted_login(email, password):
+  if not email.lower().endswith(ALLOWED_DOMAIN):
+   raise Exception(f"Only emails ending in {ALLOWED_DOMAIN} are allowed.")
+  return login_with_email(email, password)

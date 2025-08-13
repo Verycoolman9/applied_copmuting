6f390ae871d5bce1dac1e935a9a7bdb1c4bcfff8
jaxavenger2007@gmail.com <jaxavenger2007@gmail.com>
2025-08-13 01:57:08 +0000

    Edited settings, StudentSignup and TeacherSignup; Deleted SignUpPage

--- client_code/TeacherSignup/__init__.py
+++ client_code/TeacherSignup/__init__.py
@@ -8,7 +8,7 @@ from anvil.tables import app_tables
 
 class TeacherSignup(TeacherSignupTemplate):
   def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    anvil.users.login_with_form()
+    print(f"This user has logged in: {anvil.users.get_user()['email']}")
+    open_form('TeacherHomePage')
